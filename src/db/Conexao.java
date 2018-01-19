775bb33c530b2ad65b7c90eb174699e538a3ddaa
William <>
2018-01-19 08:13:40 -0200

    Criação dos pacotes, Classe de Conexao e Submenus

---
+++ src/db/Conexao.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import  java.sql.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexao {
+   public Statement stm;
+   public ResultSet rs;
+  
+   private String driver="com.mysql.jdbc.Driver";
+   private String caminho="jdbc:mysql://localhost:3306/dbtrw";
+   private String usuario="root";
+   private String senha="";
+   public Connection conn;
+
+    public void conexao(){
+       try {
+           System.setProperty("jdbc.Drivers", driver);
+           conn=DriverManager.getConnection(caminho, usuario, senha);
+           //JOptionPane.showMessageDialog(null, "Conectado com Sucesso !");
+       } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Erro ao se Conectar com o Banco: "+ex);
+       }
+   }
+    
+    public void executaSQL(String sql){
+       try {
+           stm=conn.createStatement(rs.TYPE_SCROLL_INSENSITIVE,rs.CONCUR_READ_ONLY);
+           rs = stm.executeQuery(sql);          
+       } catch (SQLException ex) {
+          JOptionPane.showMessageDialog(null, "erro ao executar SQL"+ex);
+       }
+      
+    }
+   
+   public void desconcta(){
+       try {
+           conn.close();
+           //JOptionPane.showMessageDialog(null, "Desconectado com Sucesso ");
+       } catch (Exception e) {
+           //JOptionPane.showMessageDialog(null, "Erro ao Desconectar: "+e);
+       }
+   }
+}
+
+
+
--- src/visao/Menu.java
+++ src/visao/Menu.java
@@ -36,12 +36,27 @@ public class Menu extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenuItem9 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+        jMenuItem10 = new javax.swing.JMenuItem();
+        jMenuItem11 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
+        jMenuItem12 = new javax.swing.JMenuItem();
+        jMenuItem13 = new javax.swing.JMenuItem();
         jMenu8 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
+        jMenuItem14 = new javax.swing.JMenuItem();
+        jMenuItem15 = new javax.swing.JMenuItem();
+        jMenuItem16 = new javax.swing.JMenuItem();
         jMenu5 = new javax.swing.JMenu();
         jMenu9 = new javax.swing.JMenu();
+        jMenuItem17 = new javax.swing.JMenuItem();
         jMenu6 = new javax.swing.JMenu();
         jMenu7 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -73,14 +88,50 @@ public class Menu extends javax.swing.JFrame {
 
         jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/note_edit.png"))); // NOI18N
         jMenu1.setText("Cadastros");
+
+        jMenuItem3.setText("Clientes");
+        jMenu1.add(jMenuItem3);
+
+        jMenuItem4.setText("Produtos");
+        jMenu1.add(jMenuItem4);
+
+        jMenuItem5.setText("Fornecedores");
+        jMenu1.add(jMenuItem5);
+
+        jMenuItem6.setText("Categorias");
+        jMenu1.add(jMenuItem6);
+
+        jMenuItem7.setText("Lanches");
+        jMenu1.add(jMenuItem7);
+
+        jMenuItem8.setText("Motoboy");
+        jMenu1.add(jMenuItem8);
+
+        jMenuItem9.setText("Usuários");
+        jMenu1.add(jMenuItem9);
+
         jMenuBar1.add(jMenu1);
 
         jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/shopping_cart_accept.png"))); // NOI18N
         jMenu2.setText("Caixa");
+
+        jMenuItem10.setText("Nova Venda");
+        jMenu2.add(jMenuItem10);
+
+        jMenuItem11.setText("Consultar Venda");
+        jMenu2.add(jMenuItem11);
+
         jMenuBar1.add(jMenu2);
 
         jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/calculator.png"))); // NOI18N
         jMenu3.setText("Financeiro");
+
+        jMenuItem12.setText("Contas a Pagar");
+        jMenu3.add(jMenuItem12);
+
+        jMenuItem13.setText("Contas a Receber");
+        jMenu3.add(jMenuItem13);
+
         jMenuBar1.add(jMenu3);
 
         jMenu8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/chart.png"))); // NOI18N
@@ -89,6 +140,16 @@ public class Menu extends javax.swing.JFrame {
 
         jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/process.png"))); // NOI18N
         jMenu4.setText("Ferramentas");
+
+        jMenuItem14.setText("Calculadora");
+        jMenu4.add(jMenuItem14);
+
+        jMenuItem15.setText("Bloco de Notas");
+        jMenu4.add(jMenuItem15);
+
+        jMenuItem16.setText("Agenda");
+        jMenu4.add(jMenuItem16);
+
         jMenuBar1.add(jMenu4);
 
         jMenu5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/help.png"))); // NOI18N
@@ -97,6 +158,10 @@ public class Menu extends javax.swing.JFrame {
 
         jMenu9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/database_up.png"))); // NOI18N
         jMenu9.setText("Backup");
+
+        jMenuItem17.setText("Backup Full SQL");
+        jMenu9.add(jMenuItem17);
+
         jMenuBar1.add(jMenu9);
 
         jMenu6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/info.png"))); // NOI18N
@@ -195,6 +260,21 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu9;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
+    private javax.swing.JMenuItem jMenuItem11;
+    private javax.swing.JMenuItem jMenuItem12;
+    private javax.swing.JMenuItem jMenuItem13;
+    private javax.swing.JMenuItem jMenuItem14;
+    private javax.swing.JMenuItem jMenuItem15;
+    private javax.swing.JMenuItem jMenuItem16;
+    private javax.swing.JMenuItem jMenuItem17;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
     // End of variables declaration//GEN-END:variables
 }
